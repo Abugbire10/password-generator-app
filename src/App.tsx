a7457e132e7418f47558224ced3fae87610adde1
Abugbire10 <162765874+Abugbire10@users.noreply.github.com>
2024-07-09 13:11:12 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -67,3 +67,33 @@ const upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     updateStrength(newPassword);
   };
 
+const copyToClipboard = () => {
+    if (password.length === 0) {
+      alert("Please generate a password first.");
+      return;
+    }
+    const textField = document.createElement("textarea");
+    textField.innerText = password;
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand("copy");
+    textField.remove();
+    alert("Password copied to clipboard");
+  };
+
+  const updateStrength = (password: string) => {
+    let strength = 0;
+    if (/[A-Z]/.test(password)) strength++;
+    if (/[a-z]/.test(password)) strength++;
+    if (/[0-9]/.test(password)) strength++;
+    if (/[^A-Za-z0-9]/.test(password)) strength++;
+    if (password.length >= 8) strength++;
+
+    if (strength < 2) {
+      setStrengthLabel("TOO WEAK!");
+      setStrengthClasses([
+        "strip weak",
+        "strip inactive",
+        "strip inactive",
+        "strip inactive",
+      ]);
